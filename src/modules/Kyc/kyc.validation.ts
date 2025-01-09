c2436477e5cdc3afe75e3f1cc850c2b97bbfbc07
Sharjil Khan <sharjilk62@gmail.com>
2025-01-09 17:01:11 +0530

    comments removed

--- src/modules/Kyc/kyc.validation.ts
+++ src/modules/Kyc/kyc.validation.ts
@@ -1,9 +1,5 @@
 import { body } from "express-validator";
 
-// export const validateKycSubmit = [
-//   body("document").notEmpty().withMessage("Document is required"),
-// ];
-
 export const validateKycUpdate = [
   body("status")
     .isIn(["Approved", "Rejected"])
