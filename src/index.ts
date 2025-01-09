13582e69a61de003a8ede0ccd1aa4908162fe9af
Sharjil Khan <sharjilk62@gmail.com>
2025-01-09 17:40:19 +0530

    cors fixes

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://kyc-client-beryl.vercel.app/",
     credentials: true,
   })
 );
